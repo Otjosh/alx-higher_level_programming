7efad00a2a29d694e4847826c0afbfdeb0d0b318
Otjosh <jotagetvo@gmail.com>
2023-12-06 11:19:53 -0800

    Update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,15 +1,3 @@
-#!/usr/bin/python3
-def complex_delete(my_dict, value):
-    tmp = my_dict.copy()
-    for k, v in tmp.items():
-        if value == v:
-            my_dict.pop(k)
-    return my_dict
-
-vi 103-python.c
-
-#include <Python.h>
-#include <object.h>
 #include <listobject.h>
 #include <bytesobject.h>
 
